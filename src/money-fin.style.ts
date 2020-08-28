958d36997c50c25d241f1464c82095e51f30370c
taeuk-gang <taeuk_kang@naver.com>
2020-08-28 22:07:32 +0900

    split style file

---
+++ src/money-fin.style.ts
@@ -0,0 +1,9 @@
+import { css } from 'lit-element';
+
+export default css`
+  :host {
+    display: inline-block;
+    position: fixed;
+    z-index: 10000;
+  }
+`;
--- src/money-fin.ts
+++ src/money-fin.ts
@@ -1,9 +1,13 @@
-import { LitElement, html, css, property, TemplateResult } from 'lit-element';
-
+import { LitElement, html, TemplateResult } from 'lit-element';
+import style from './money-fin.style';
 import { dragElement } from './utils/draggable';
 
 export class MoneyFin extends LitElement {
-  @property({ type: String }) page = 'main';
+  static styles = style;
+
+  render(): TemplateResult {
+    return html` <div>TEST</div> `;
+  }
 
   protected firstUpdated(): void {
     this.init();
