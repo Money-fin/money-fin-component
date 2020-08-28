e09a0dfc387b51c923c4566344286039ecde2968
taeuk-gang <taeuk_kang@naver.com>
2020-08-28 22:52:01 +0900

    #1 design fab button

--- src/money-fin.ts
+++ src/money-fin.ts
@@ -1,12 +1,23 @@
 import { LitElement, html, TemplateResult } from 'lit-element';
 import style from './money-fin.style';
+import fabStyle from './style/fab.style';
 import { dragElement } from './utils/draggable';
 
 export class MoneyFin extends LitElement {
-  static styles = style;
+  static styles = [fabStyle, style];
 
   render(): TemplateResult {
-    return html` <div>TEST</div> `;
+    return html`
+      <div class="fab-wrapper">
+        <input id="fabCheckbox" type="checkbox" class="fab-checkbox" />
+        <label class="fab" for="fabCheckbox">
+          <span class="fab-dots fab-dots-1"></span>
+          <span class="fab-dots fab-dots-2"></span>
+          <span class="fab-dots fab-dots-3"></span>
+        </label>
+        <div class="fab-wheel">Test</div>
+      </div>
+    `;
   }
 
   protected firstUpdated(): void {
